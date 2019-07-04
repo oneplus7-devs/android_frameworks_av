2f1c52814e94a79cb081d8e27b2e84b9d78bba45
Peter Cai <peter@typeblog.net>
2019-07-04 09:05:35 +0800

    CameraService: sync with gerrit

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -1446,6 +1446,11 @@ Status CameraService::connectHelper(const sp<CALLBACK>& cameraCb, const String8&
         } else {
             // Otherwise, add client to active clients list
             finishConnectLocked(client, partial);
+
+            sp<ICameraMotor> cameraMotor = ICameraMotor::getService();
+            if (cameraMotor != nullptr) {
+                cameraMotor->onConnect(std::stoi(cameraId.string()));
+            }
         }
     } // lock is destroyed, allow further connect calls
 
@@ -2191,9 +2196,9 @@ binder::Status CameraService::BasicClient::disconnect() {
     }
     mDisconnected = true;
 
-    sp<ICameraMotor> motor_service = ICameraMotor::getService();
-    if (motor_service != nullptr) {
-        motor_service->onDisconnect(std::stoi(mCameraIdStr.string()));
+    sp<ICameraMotor> cameraMotor = ICameraMotor::getService();
+    if (cameraMotor != nullptr) {
+        cameraMotor->onDisconnect(std::stoi(mCameraIdStr.string()));
     }
 
     sCameraService->removeByClient(this);
